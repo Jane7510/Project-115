fab63272adf5be3009c5182ea8e5d8a5fbcd7adf
Jane7510 <80323076+Jane7510@users.noreply.github.com>
2022-02-05 12:10:08 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,32 @@
+noseX = 0;
+noseY = 0;
+
+function preload() {
+    clown_nose = loadImage("https://i.postimg.cc/3NWJq1sF/Clown-nose-large.png")
+}
+
+function setup() {
+    canvas = createCanvas(300, 300);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.size(300, 300);
+    video.hide();
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+function modelLoaded() {
+    console.log("PoseNet is initialized!")
+}
+
+function gotPoses(results) {
+    if(results.length > 0) {
+
+        console.log(results);
+        noseX = results[0].pose.nose.x - 25;
+        noseY = results[0].pose.nose.y - 20;
+        console.log("Nose x = " + noseX)  ;
+        console.log("Nose y = " + noseY);
+    }
+}   
